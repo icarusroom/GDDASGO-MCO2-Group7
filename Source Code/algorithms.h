3d7bf141b1884421ace6890ded718bb382a8ce87
Gray Jacinto <69553576+icarusroom@users.noreply.github.com>
2023-03-11 08:37:17 +0800

    Update algorithms.h

--- Source Code/algorithms.h
+++ Source Code/algorithms.h
@@ -109,15 +109,21 @@ void infixToPostfix(char *infix, char *postfix) {
 }
 
 int evaluatePostfix(char *postfix) {
-	return evaluatePostfixWithoutQueue(postfix);
+	//return evaluatePostfixWithoutQueue(postfix);
 	/*
 	//NOT USING STACK/QUEUE. IT ADDS MORE STEPS TO STORE STRING THEN RECONVERT TO INT. JUST CONVERT AND USE INT ARRAY DIRECTLY
 	int n_Temp = 0;			//TEMPORARY INT STORAGE
 	int n_L, n_R;			//THE LEFT AND RIGHT INT FOR COMPUTATION
-	int a_Operands[257];	//THE OPERANDS CONTAINER. CAN BE SWITCHED FOR QUEUE
+	queue* p_Operands = createQueue(257);	//THE OPERANDS CONTAINER. CAN BE SWITCHED FOR QUEUE
 	int n_Pointer = -1;		//THE STARTING POINTER INDEX
 
 	int i = 0;	//THE CHAR INDEX
+
+	//turn string into an integer array
+	int n_Length = strlen(postfix);
+	int *p_Result = (int*)malloc(n_Length*sizeof(int));
+	for(i=0; i<n_Length; i++)
+	p_Result[i] = postfix[i] - 48;
 
 
 	//READ THE STRING
@@ -129,7 +135,8 @@ int evaluatePostfix(char *postfix) {
 			//CHECK IF IT'S THE LAST DIGIT THEN STORE
 			if(!isDigit(postfix[i+1])){
 				n_Pointer++;
-				a_Operands[n_Pointer] = n_Temp;
+				//a_Operands[n_Pointer] = n_Temp;
+				enqueue(&p_Operands, (n_Temp+i));
 			}
 			//ELSE PREPARE TO ADD MORE DIGITS
 			else{
@@ -144,9 +151,9 @@ int evaluatePostfix(char *postfix) {
 
 		//IF AN OPERATOR
 		else{
-			n_R = a_Operands[n_Pointer];	//SET THE CURRENT AS RIGHT
-			n_Pointer--;
-			n_L = a_Operands[n_Pointer];	//SET THE PREVIOUS AS LEFT
+			n_R = tail(p_Operands);//a_Operands[n_Pointer];	//SET THE CURRENT AS RIGHT
+			//n_Pointer--;
+			n_L = head(p_Operands);//a_Operands[n_Pointer];	//SET THE PREVIOUS AS LEFT
 
 
 			switch(postfix[i]){
@@ -160,6 +167,10 @@ int evaluatePostfix(char *postfix) {
 					n_Temp = n_L * n_R;
 					break;
 				case '/':
+					if (n_R == 0){
+						//ERROR FOR DIVIDING BY ZERO
+						return -999;
+					}
 					n_Temp = n_L / n_R;
 					break;
 				case '&':
@@ -168,7 +179,6 @@ int evaluatePostfix(char *postfix) {
 					i++;	//AVOID READING 2nd &
 					break;
 				case '|':
-					//||
 					n_Temp = n_L || n_R;
 					i++;	//AVOID READING 2nd |
 					break;
@@ -213,12 +223,152 @@ int evaluatePostfix(char *postfix) {
 					break;
 				default:
 			}
-			printf("(%d)", n_Temp);
-			a_Operands[n_Pointer] = n_Temp;		//OVERRIDE THE LEFT'S CONTAINER
+			//printf("(%d)", n_Temp);		//RESULT OF EVERY CALC
+			enqueue(&p_Operands, n_Temp); //a_Operands[n_Pointer] = n_Temp;		//OVERRIDE THE LEFT'S CONTAINER
 		}
 		i++;
 	}
 
-	return a_Operands[0];	//SINCE ALWAYS OVERRIDING THE LEFT, FINAL VALUE IS AT THE BOTTOM
-	*/
+	dequeue(&p_Operands);	//SINCE ALWAYS OVERRIDING THE LEFT, FINAL VALUE IS AT THE BOTTOM
+*/
+	
+	int n_Temp = 0;                          //temp int storage
+	char c_Temp;                        
+	//char c_TempNext;
+	int n_TempResult;
+	int n_L, n_R;                            //L & R int for computation
+	queue* p_Operators = createQueue(257);   //Creates a queue for operators
+	stack* p_Operands = createStack(257);    //Creates a stack for operands
+	stack* p_TempResult = createStack(257); //Stack if postfix cannot be solved in one go
+	int n_Pointer = 0;                       //starting pointer index
+
+	int i = 0;	//index for postfix
+
+	//loops until postfix is completely solved
+	while(postfix[i] != '\0'){
+
+		//keeps looping until the postfix is an operator followed by an operand
+		while(!(isDigit(postfix[i]) && !isDigit(postfix[i-2]))){
+			
+			//if postfix value in i is a digit, add it to the stack
+			if (isDigit(postfix[i])){
+				n_Temp += postfix[i] - 48;
+
+				//if slot after current digit is a digit, loop to keep adding values.
+				while(isDigit(postfix[i+1])){
+					i++;
+					n_Temp *= 10;
+					n_Temp += postfix[i] - 48;
+				}
+
+				push(&p_Operands, n_Temp);
+			}
+
+			//if postfix value is a space, disregard it
+			else if (postfix[i] == ' '){
+
+			}
+
+			//if postfic value in i is an operator, add it to the queue
+			else{
+				c_Temp = postfix[i];
+				
+				enqueue(&p_Operators, c_Temp);
+			}
+
+		//increments index to move on to the next slot in the postfix array.
+		i++;
+		}
+
+		//evaluation step
+		//loops until all values in the stack and queue are used up
+		while(!queueEmpty(p_Operators) && !stackEmpty(p_Operands)){
+			n_R = pop(&p_Operands);
+			n_L = pop(&p_Operands);
+
+			switch(dequeue(&p_Operators)){
+				case '+':
+					n_Temp = n_L + n_R;
+					break;
+				case '-':
+					n_Temp = n_L - n_R;
+					break;
+				case '*':
+					n_Temp = n_L * n_R;
+					break;
+				case '/':
+					if (n_R == 0){
+						//ERROR FOR DIVIDING BY ZERO
+						return -999;
+					}
+					n_Temp = n_L / n_R;
+					break;
+				case '&':
+					//&&
+					n_Temp = n_L && n_R;
+					i++;	//AVOID READING 2nd &
+					break;
+				case '|':
+					n_Temp = n_L || n_R;
+					i++;	//AVOID READING 2nd |
+					break;
+				case '=':
+					//==
+					n_Temp = n_L == n_R;
+					i++;	//AVOID READING 2nd =
+					break;
+				case '!':
+					//!=
+					if (postfix[i+1] == '='){
+						n_Temp = n_L != n_R;
+						i++;	//AVOID READING =
+					}
+					//!
+					else{
+						n_Temp = !n_R;
+						n_Pointer++;	//MOVE THE POINTER BACK TO USE RIGHT'S CONTAINER.
+					}
+					break;
+				case '<':
+					//<=
+					if(postfix[i+1] == '='){
+						n_Temp = n_L <= n_R;
+						i++;	//AVOID READING =
+					}
+					//<
+					else{
+						n_Temp = n_L < n_R;
+					}
+					break;
+				case '>':
+					//>=
+					if(postfix[i+1] == '='){
+						n_Temp = n_L >= n_R;
+						i++;	//AVOID READING =
+					}
+					//>
+					else{
+						n_Temp = n_L > n_R;
+					}
+					break;
+				default:
+			}
+		}
+			
+			
+			push(p_TempResult, n_Temp);		//OVERRIDE THE LEFT'S CONTAINER
+		
+	}
+
+	return p_Operands[0];	//SINCE ALWAYS OVERRIDING THE LEFT, FINAL VALUE IS AT THE BOTTOM
+	
 }
+
+//supposedly for the operators with 2 slots
+/*
+				//if operator has more than 1 slot in the post fix
+				if(!isDigit(postfix[i+1]) && postfix[i+1] != ' '){
+					i++;
+					strcpy(c_TempNext, postfix[i]); 
+					strcat(c_Temp, c_TempNext);
+				}*/
